32c8a1edf001e20491fc5fdebc8e0636d5d9ad34
Ondřej Čertík <ondrej.certik@gmail.com>
2016-06-07 21:55:42 +0200

    Fix a bug in prettify.py

--- prettify.py
+++ prettify.py
@@ -6,12 +6,12 @@ filename = "dict.csv"
 
 rows = []
 
-with open(filename) as csvfile:
+with open(filename, newline='') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         rows.append([x.strip() for x in row])
 
-with open(filename, 'w') as csvfile:
-    writer = csv.writer(csvfile)
+with open(filename, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, lineterminator='\n')
     for row in rows:
         writer.writerow(row)
